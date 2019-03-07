03c00756aaf8af3be1a95c2cb590f06a5e547447
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-07 21:35:10 +0800

    feat: add user layers modules for collecting reusable combination neural network layer

---
+++ modelzoo/nn/layers.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+    def forward(self, x):
+        return x.view(x.size(0), -1)
