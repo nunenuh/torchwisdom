c9d725bc122d3e735e36c46f796b0410c4fa9465
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-08 02:25:25 +0800

    feat: add ContrastiveLoss for siamese loss

--- modelzoo/nn/loss.py
+++ modelzoo/nn/loss.py
@@ -38,6 +38,22 @@ class BCEDiceLoss(nn.Module):
         dice_loss = self.dice_loss(input, target)
         return bce_loss + dice_loss
 
+# taken from https://gist.github.com/harveyslash/725fcc68df112980328951b3426c0e0b#file-contrastive-loss-py
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive
+
 
 
 
