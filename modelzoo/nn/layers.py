b5dfb80c9b52ec81fb174b1c2e7a92eeb22436ac
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-10 02:13:23 +0800

    feat: add AdaptiveConcatPool2d and Lamda

--- modelzoo/nn/layers.py
+++ modelzoo/nn/layers.py
@@ -7,3 +7,23 @@ class Flatten(nn.Module):
         super(Flatten, self).__init__()
     def forward(self, x):
         return x.view(x.size(0), -1)
+
+# got from fastai
+class AdaptiveConcatPool2d(nn.Module):
+    "Layer that concats `AdaptiveAvgPool2d` and `AdaptiveMaxPool2d`."
+    def __init__(self, sz:int=None):
+        "Output will be 2*sz or 2 if sz is None"
+        super().__init__()
+        sz = sz or 1
+        self.ap,self.mp = nn.AdaptiveAvgPool2d(sz), nn.AdaptiveMaxPool2d(sz)
+    def forward(self, x): return torch.cat([self.mp(x), self.ap(x)], 1)
+
+#got from fastai
+class Lambda(nn.Module):
+    "An easy way to create a pytorch layer for a simple `func`."
+    def __init__(self, func):
+        "create a layer that simply calls `func` with `x`"
+        super().__init__()
+        self.func=func
+
+    def forward(self, x): return self.func(x)
