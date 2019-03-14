1d67a4552088642c144cff67af9ba4546cff903b
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-14 09:10:00 +0800

    fix: change variable output_channel to be class attribute

--- torchwisdom/models/mobilenet.py
+++ torchwisdom/models/mobilenet.py
@@ -207,10 +207,10 @@ class MobileNetV2(nn.Module):
                 input_channel = output_channel
         self.features = nn.Sequential(*layers)
         # building last several layers
-        output_channel = _make_divisible(1280 * width_mult, 8) if width_mult > 1.0 else 1280
-        self.conv = conv_1x1_bn(input_channel, output_channel)
+        self.output_channel = _make_divisible(1280 * width_mult, 8) if width_mult > 1.0 else 1280
+        self.conv = conv_1x1_bn(input_channel, self.output_channel)
         self.avgpool = nn.AvgPool2d(input_size // 32, stride=1)
-        self.classifier = nn.Linear(output_channel, num_classes)
+        self.classifier = nn.Linear(self.output_channel, num_classes)
 
         self._initialize_weights()
 
