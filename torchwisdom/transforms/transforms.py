9657c67af4d9c8db01e3dee7f22c7e3a22441bb3
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-22 07:38:00 +0800

    feat: add PairCompose and PairResize

--- torchwisdom/transforms/transforms.py
+++ torchwisdom/transforms/transforms.py
@@ -1,25 +1,40 @@
 import torch
+from PIL import Image
+import PIL
+import collections
+import torchvision.transforms.functional as F
+import torchvision
+from torchvision import transforms
 
+__all__ = ['PairCompose','PairResize']
 
-class PairCompose(object):
-    """
+_pil_interpolation_to_str = {
+    Image.NEAREST: 'PIL.Image.NEAREST',
+    Image.BILINEAR: 'PIL.Image.BILINEAR',
+    Image.BICUBIC: 'PIL.Image.BICUBIC',
+    Image.LANCZOS: 'PIL.Image.LANCZOS',
+    Image.HAMMING: 'PIL.Image.HAMMING',
+    Image.BOX: 'PIL.Image.BOX',
+}
 
-    """
-
+class PairCompose(transforms.Compose):
     def __init__(self, transforms):
-        self.transforms = transforms
+        super(PairCompose, self).__init__(transforms)
 
     def __call__(self, img1, img2):
         for t in self.transforms:
             img1, img2 = t(img1, img2)
         return img1, img2
 
-    def __repr__(self):
-        format_string = self.__class__.__name__ + '('
-        for t in self.transforms:
-            format_string += '\n'
-            format_string += '    {0}'.format(t)
-        format_string += '\n)'
-        return format_string
+
+class PairResize(transforms.Resize):
+    def __init__(self, size, interpolation=Image.BILINEAR):
+       super(PairResize, self).__init__(size, interpolation)
+
+    def __call__(self, img1, img2):
+        img1 = F.resize(img1, self.size, self.interpolation)
+        img2 = F.resize(img2, self.size, self.interpolation)
+        return img1, img2
+
 
 
