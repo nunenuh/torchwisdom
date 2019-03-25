1f9de8fce55f9be08b4a711609da8f37313e9753
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-25 23:15:54 +0800

    feat: add TrainValidTestSplit class to module split

--- torchwisdom/utils/data/split.py
+++ torchwisdom/utils/data/split.py
@@ -91,3 +91,81 @@ class TrainValidSplit(object):
                     raise ValueError('mode only accept value "cp" or "mv"')
 
 
+class TrainValidTestSplit(TrainValidSplit):
+    def __init__(self, src_path, val_size=0.2, test_size=0.1):
+        super(TrainValidTestSplit, self).__init__(src_path, val_size=val_size)
+        self.test_size = test_size
+
+    def _split_map(self):
+        bfiles_map = self.src_files_map
+        valid_files = {}
+        train_files = {}
+        test_files = {}
+
+        for key in bfiles_map.keys():
+            files = bfiles_map[key]
+            n = len(bfiles_map[key])
+            nd = [i for i in range(n)]
+            test_sampling_size = int( n * self.test_size)
+            valid_sampling_size = int((n-test_sampling_size) * self.val_size)
+            test_set = set(random.sample(nd, test_sampling_size))
+            train_val_set = set(nd) - test_set
+            valid_set = set(random.sample(list(train_val_set), valid_sampling_size))
+            train_set = train_val_set - valid_set
+
+            for vidx in list(valid_set):
+                if key in valid_files.keys():
+                    valid_files[key].append(files[vidx])
+                else:
+                    valid_files.update({key :[files[vidx]]})
+
+            for tidx in list(train_set):
+                if key in train_files.keys():
+                    train_files[key].append(files[tidx])
+                else:
+                    train_files.update({key :[files[tidx]]})
+
+            for tidx in list(test_set):
+                if key in test_files.keys():
+                    test_files[key].append(files[tidx])
+                else:
+                    test_files.update({key :[files[tidx]]})
+
+
+        return train_files, valid_files, test_files
+
+
+    def execute(self, dst_path:str, mode:str='cp'):
+        self.train_files_map, self.valid_files_map, self.test_files_map = self._split_map()
+        if dst_path:
+            self.dst_path = pathlib.Path(dst_path)
+            self.dst_path.mkdir(parents=True, exist_ok=True)
+
+            train_path = self.dst_path.joinpath('train')
+            train_path.mkdir(parents=True, exist_ok=True)
+
+            valid_path = self.dst_path.joinpath('valid')
+            valid_path.mkdir(parents=True, exist_ok=True)
+
+            test_path = self.dst_path.joinpath('test')
+            test_path.mkdir(parents=True, exist_ok=True)
+        else:
+            raise ValueError("dst_path cannot be None, it must has value for destination directory")
+
+        for key in self.src_files_map.keys():
+            train_path.joinpath(key).mkdir(parents=True, exist_ok=True)
+            valid_path.joinpath(key).mkdir(parents=True, exist_ok=True)
+            test_path.joinpath(key).mkdir(parents=True, exist_ok=True)
+
+
+        TrainValidSplit.copy_move(train_path, self.train_files_map, mode=mode)
+        TrainValidSplit.copy_move(valid_path, self.valid_files_map, mode=mode)
+        TrainValidSplit.copy_move(test_path, self.test_files_map, mode=mode)
+
+
+
+if __name__ == '__main__':
+    src = '/data/att_faces'
+    dst = '/data/att_faces_new'
+    rs = TrainValidTestSplit(src_path=src, val_size=0.3, test_size=0.1)
+    rs.execute(dst, mode='cp')
