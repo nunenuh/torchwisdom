508d9310564b6c10ac39876976b0a1bc0844a877
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-03-31 22:44:42 +0800

    feat: add AverageMetrics for responding request issue #6

--- torchwisdom/metrics/metrics.py
+++ torchwisdom/metrics/metrics.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from torchwisdom.callback import Callback
+from typing import *
 
 # taken from https://github.com/pytorch/examples/blob/master/imagenet/main.py
 class AverageMeter(object):
@@ -20,6 +21,37 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+
+class AverageMetrics(object):
+    def __init__(self):
+        self.restart()
+
+    def restart(self):
+        self.value = 0
+        self.values = []
+        self.mean = 0
+        self.std = 0
+
+    def update(self, val):
+        self.value = val
+        self.values.append(val)
+        tensor = torch.Tensor(self.values)
+        self.mean = tensor.mean().item()
+        self.std = tensor.std().item()
+
+
+class AverageMeterCallback(Callback):
+    def __init__(self):
+        self.avm = AverageMeter()
+
+    def on_epoch_begin(self, **kwargs: Any):
+        self.avm.reset()
+
+    def on_batch_begin(self, val, batch_size):
+        self.avm.update(val, batch_size)
+
+
 
 
 class AccuracyTopK(object):
