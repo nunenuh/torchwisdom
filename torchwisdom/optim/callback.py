d4b9f31d2476ea5d869a9c807db184457b973467
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-04-14 04:16:30 +0800

    refactor: change code in ReduceLROnPlateauCallback

--- torchwisdom/optim/callback.py
+++ torchwisdom/optim/callback.py
@@ -23,6 +23,7 @@ class _SchedulerCallback(Callback):
         self.statemgr: StateManager = None
         self.optimizer: optim.Optimizer = None
         self.scheduler: lr_scheduler.ReduceLROnPlateau = None
+        self.mode_name = 'loss'
 
     def on_epoch_end(self, *args: Any, **kwargs: Any) -> None:
         loss: torch.Tensor = kwargs.get('loss')
@@ -43,29 +44,29 @@ class StepLRCallback(_LRSchedulerCallback):
 
 class MultiStepLRCallback(_LRSchedulerCallback):
     def __init__(self,  milestones: list, gamma: float = 0.1,
-                 last_epcoch: int = -1, *args: Any, **kwargs: Any)->Any:
+                 last_epoch: int = -1, *args: Any, **kwargs: Any) -> None:
         super(MultiStepLRCallback, self).__init__(*args, **kwargs)
         self.milestones = milestones
         self.gamma = gamma
-        self.last_epoch = last_epcoch
+        self.last_epoch = last_epoch
 
     def on_fit_begin(self, *args: Any, **kwargs: Any) -> None:
-        self.scheduler = lr_scheduler.MultiStepLR(self.optimizer, self.milestones, self.gamma, self.last_epcoch)
+        self.scheduler = lr_scheduler.MultiStepLR(self.optimizer, self.milestones, self.gamma, self.last_epoch)
 
 
 class LambdaLRCallback(_LRSchedulerCallback):
-    def __init__(self,  lr_lambda, last_epcoch: int = -1,
+    def __init__(self,  lr_lambda, last_epoch: int = -1,
                  *args: Any, **kwargs: Any) -> None:
         super(LambdaLRCallback, self).__init__(*args, **kwargs)
-        self.lr_lamda = lr_lambda
-        self.last_epoch = last_epcoch
+        self.lr_lambda = lr_lambda
+        self.last_epoch = last_epoch
 
     def on_fit_begin(self, *args: Any, **kwargs: Any) -> None:
-        self.scheduler = lr_scheduler.LambdaLR(self.optimizer, self.lr_lambda, self.last_epcoch)
+        self.scheduler = lr_scheduler.LambdaLR(self.optimizer, self.lr_lambda, self.last_epoch)
 
 
 class ExponentialLRCallback(_LRSchedulerCallback):
-    def __init__(self,  gamma:float, last_epoch:int = -1, *args:Any, **kwargs:Any)->None:
+    def __init__(self,  gamma:float, last_epoch:int = -1, *args:Any, **kwargs:Any) -> None:
         super(ExponentialLRCallback, self).__init__(*args, **kwargs)
         self.gamma = gamma
         self.last_epoch = last_epoch
@@ -87,27 +88,23 @@ class CosineAnnealingLRCallback(_LRSchedulerCallback):
 
 
 class ReduceLROnPlateauCallback(_SchedulerCallback):
-    def __init__(self, mode: str = 'min', factor: float = 0.1,
-                patience: int = 10, verbose: bool = False, threshold: float = 1e-4,
-                threshold_mode: str = 'rel', cooldown: int = 0, min_lr: Union[float, list] = 0,
-                eps: float = 1e-8, *args, **kwargs) -> Any:
+    def __init__(self, mode: str = 'min', factor: float = 0.1, patience: int = 10,
+                 verbose: bool = False, *args, **kwargs) -> None:
         super(ReduceLROnPlateauCallback, self).__init__(*args, **kwargs)
-        self.mode = mode
-        self.factor = factor
-        self.patience = patience
-        self.verbose = verbose
-        self.threshold = threshold
-        self.threshold_mode = threshold_mode
-        self.cooldown = cooldown
-        self.min_lr = min_lr
-        self.eps = eps
+        self.mode: str = mode
+        self.factor: float = factor
+        self.patience: int = patience
+        self.verbose: bool = verbose
+        self.threshold: float = kwargs.get('threshold', 1e-4)
+        self.threshold_mode: str = kwargs.get('threshold_mode', 'rel')
+        self.cooldown: int = kwargs.get('cooldown', 0)
+        self.min_lr: Union[float, list] = kwargs.get('min_lr', 0)
+        self.eps: float = kwargs.get('eps', 1e-8)
 
     def on_fit_begin(self, *args: Any, **kwargs: Any) -> None:
         self.scheduler = lr_scheduler.ReduceLROnPlateau(self.optimizer, self.mode, self.factor,
                                                         self.patience, self.verbose, self.threshold,
                                                         self.threshold_mode, self.cooldown, self.min_lr, self.eps)
-
-
 
 
 if __name__ == '__main__':
