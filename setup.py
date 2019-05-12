6c82e3776d61653c97d45faf529b9f4c1cf0fa32
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-05-12 22:29:30 +0800

    chore: update setup.py

---
+++ setup.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+from os import path
+here = path.abspath(path.dirname(__file__))
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='torchwisdom',
+    version='0.0.2-dev1',
+    description='A High Level API for PyTorch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/nunenuh/torchwisdom',
+    author='Lalu Erfandi Maula Yusnu',
+    author_email='nunenuh@gmail.com',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='sample setuptools development',  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+
+    python_requires='>=3.5, <=3.6',
+    install_requires=['pytorch'],
+
+    extras_require={  # Optional
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/nunenuh/torchwisdom/issues',
+        # 'Funding': 'https://donate.pypi.org',
+        # 'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/nunenuh/torchwisdom/',
+    },
+)
