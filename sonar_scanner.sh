5e84eed156d53b396add95c2a2ca8ec0100c5e53
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-05-12 15:27:53 +0800

    chore: add sonar_scanner script

---
+++ sonar_scanner.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sonar-scanner   -Dsonar.projectKey=torchwisdom   -Dsonar.organization=torchwisdom   -Dsonar.sources=.   -Dsonar.host.url=https://sonarcloud.io   -Dsonar.login=ccda30adf302b56d28c8e73ff7e441e79eac2b90
