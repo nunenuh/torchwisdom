62a51cd82c8b5bba99460a21c5b52b679662595b
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-05-29 16:42:22 +0800

    feat: add one line cardinality for dice coeff

--- torchwisdom/core/nn/loss.py
+++ torchwisdom/core/nn/loss.py
@@ -9,9 +9,9 @@ import torch.nn.functional as F
 def dice_coeff(input, target, smooth=1.):
     input_flat = input.view(-1)
     target_flat = target.view(-1)
-
     intersection = (input_flat * target_flat).sum()
-    return (2. * intersection + smooth) / (input_flat.sum() + target_flat.sum() + smooth)
+    cardinality = input_flat.sum() + target_flat.sum()
+    return (2. * intersection + smooth) / (cardinality + smooth)
 
 
 class DiceLoss(nn.Module):
