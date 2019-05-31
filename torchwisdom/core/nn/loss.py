7d99b11787e238fb3225db2b35ab4fec23c3d880
Lalu Erfandi Maula Yusnu <nunenuh@gmail.com>
2019-06-01 06:21:06 +0800

    refactor: rebuild ContrastiveLoss with more clear and readable meaning

--- torchwisdom/core/nn/loss.py
+++ torchwisdom/core/nn/loss.py
@@ -1,45 +1,43 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from ..nn import functional as N
+
 
 # inspiration from
 # https://github.com/pytorch/pytorch/issues/1249
 # github.com/jeffwen/road_building_extraction/blob/master/src/utils/core.py
 # and other source
-def dice_coeff(input, target, smooth=1.):
-    input_flat = input.view(-1)
-    target_flat = target.view(-1)
-    intersection = (input_flat * target_flat).sum()
-    cardinality = input_flat.sum() + target_flat.sum()
-    return (2. * intersection + smooth) / (cardinality + smooth)
-
-
 class DiceLoss(nn.Module):
-    def __init__(self, sigmoid_norm=False):
+    def __init__(self, sigmoid_norm: bool = False, softmax_norm: bool = False):
         super(DiceLoss, self).__init__()
         self.sigmoid_norm = sigmoid_norm
+        self.softmax_norm = softmax_norm
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, input, target):
+    def forward(self, y_pred: torch.Tensor, y_true: torch.Tensor):
         if self.sigmoid_norm:
-            input = torch.sigmoid(input)
-        else:
-            softmax = nn.Softmax(dim=1)
-            input = softmax(input)
-        return dice_coeff(input, target)
+            y_pred = torch.sigmoid(y_pred)
+        if self.softmax_norm:
+            y_pred = self.softmax(y_pred)
+        return N.dice_coeff(y_pred, y_true)
 
 
 class SoftDiceLoss(nn.Module):
-    def __init__(self, sigmoid_norm=False):
+    def __init__(self, sigmoid_norm: bool = False, softmax_norm: bool = False):
         super(SoftDiceLoss, self).__init__()
         self.sigmoid_norm = sigmoid_norm
+        self.softmax_norm = softmax_norm
+        self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, input, target):
+    def forward(self, y_pred: torch.Tensor, y_true) -> torch.Tensor:
         if self.sigmoid_norm:
-            input = torch.sigmoid(input)
+            y_pred = torch.sigmoid(y_pred)
         else:
-            softmax = nn.Softmax(dim=1)
-            input = softmax(input)
-        return 1 - dice_coeff(input, target)
+            y_pred = self.softmax(y_pred)
+        return 1 - N.dice_coeff(y_pred, y_true)
+
+
 # inspiration from
 # github.com/jeffwen/road_building_extraction/blob/master/src/utils/core.py
 # and other source
@@ -56,24 +54,22 @@ class BCEDiceLoss(nn.Module):
         dice_loss = self.dice_loss(input, target)
         return bce_loss + dice_loss
 
-# taken from https://gist.github.com/harveyslash/725fcc68df112980328951b3426c0e0b#file-contrastive-loss-py
+
+# inspired from https://gist.github.com/harveyslash/725fcc68df112980328951b3426c0e0b#file-contrastive-loss-py
 class ContrastiveLoss(torch.nn.Module):
     """
     Contrastive loss function.
     Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
     """
+
     def __init__(self, margin=2.0):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
-    def forward(self, output1, output2, label):
-        euclidean_distance = F.pairwise_distance(output1, output2)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
-                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+    def forward(self, y_predz, y_predj, y_true):
+        dw = F.pairwise_distance(y_predz, y_predj)
+        loss_similar = (1 - y_true) * torch.mean(torch.pow(dw, 2))
+        loss_dissimilar = y_true * torch.mean(torch.pow(torch.clamp(self.margin - dw, min=0.0), 2))
+        loss_contrastive = loss_similar + loss_dissimilar
+
         return loss_contrastive
-
-
-
-
-
-
